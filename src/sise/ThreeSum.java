4de026bda20234cf6ba382604fbf30228cfaa7b9
z0j0js <783155876@qq.com>
2020-05-02 09:16:36 +0800

    第一题：两数之和

---
+++ src/sise/ThreeSum.java
@@ -0,0 +1,5 @@
+package sise;
+
+public class ThreeSum {
+
+}
---
+++ src/sise/TwoSum.java
@@ -0,0 +1,22 @@
+package sise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args){
+        System.out.println(TwoSum.twoSum(new int[] {2,7,11,15}, 9));
+    }
+}
