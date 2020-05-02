8236c5426904efa6771dc9b1471c13e5e6efbf46
z0j0js <783155876@qq.com>
2020-05-02 09:32:37 +0800

    第一题：两数之和

--- src/sise/TwoSum.java
+++ src/sise/TwoSum.java
@@ -1,13 +1,17 @@
 package sise;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 两数之和
+ * 题目：两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
  */
+
 public class TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
+    private static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
@@ -20,6 +24,6 @@ public class TwoSum {
     }
 
     public static void main(String[] args){
-        System.out.println(TwoSum.twoSum(new int[] {2,7,11,15}, 9));
+        System.out.println(Arrays.toString(TwoSum.twoSum(new int[] {2,7,11,15}, 9)));
     }
 }
